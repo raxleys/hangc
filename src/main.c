e8160924e0d6b3c24bb7f8b1ddddca58f16792b4
raxleys <raxleys@gmail.com>
2025-03-19 20:32:14 -0400

    Add word list parsing

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 // Path from project root
 #define DATA_PATH "./data/"
@@ -32,6 +33,10 @@ typedef struct Images {
 Images *parse_images(const char *buf);
 void free_images(Images **images);
 
+// Returns number of words (not buffersize allocated)
+size_t parse_wordlist(string ***wordlist, char *raw_wordlist);
+void free_words(string ***wordlist, size_t nwords);
+
 string *string_dupn(const char *buf, size_t n);
 void string_free(string **str);
 strlist *strlist_new();
@@ -39,17 +44,35 @@ void strlist_free(strlist **list);
 
 int main(int argc, char *argv[])
 {
+    // Hush gcc!
+    (void)argc;
+    (void)argv;
+
+    // ASCII images
     char *raw_images = read_file(IMAGES_F);
     Images *images = parse_images(raw_images);
 
-    for (strlist *im = images->list; im; im = im->next)
-        puts(im->str->buf);
+    // Print images
+    /* for (strlist *im = images->list; im; im = im->next) */
+        /* puts(im->str->buf); */
 
     puts("Current image:");
     puts(images->active->str->buf);
 
+    // Wordlist
+    char *raw_wordlist = read_file(WORD_BANK_F);
+    string **words;
+    size_t nwords = parse_wordlist(&words, raw_wordlist);
+
+    // Print words
+    /* for (size_t i = 0; i < nwords; i++) */
+        /* printf("Word #%zu: %s\n", i + 1, words[i]->buf); */
+
+    // Free memory
+    free_words(&words, nwords);
     free_images(&images);
     free(raw_images); // TODO: free earlier?
+    free(raw_wordlist);
     return 0;
 }
 
@@ -99,7 +122,6 @@ Images *parse_images(const char *buf)
             end++;
 
         if (*p) {
-            /* Image *image = image_from(buf + start, end - start); */
             string *image = string_dupn(buf + start, end - start);
             assert(image != NULL && "Malloc failed");
 
@@ -174,4 +196,57 @@ void free_images(Images **images)
     strlist_free(&(*images)->list);
     free(*images);
     *images = NULL;
+}
+
+size_t parse_wordlist(string ***wordlist, char *raw_wordlist)
+{
+    // Count # of newlines.
+    // A buffer that can fit that can be guaranteed to fit enough words
+    size_t nwords = 0;
+    for (char *p = raw_wordlist; *p != '\0'; p++)
+        if (*p == '\n') nwords++;
+    
+    size_t words_len = 0;
+    string **words = malloc(nwords * sizeof(*words));
+    assert(words != NULL && "Malloc failed");
+
+    char *p = raw_wordlist;
+    size_t start = 0;
+    size_t end = 0;
+
+    while (true) {
+        for (; *p != '\0' && *p != '\n'; p++)
+            end++;
+
+        if (*p == '\0')
+            break;
+
+        string *w = string_dupn(raw_wordlist + start, end - start);
+        printf("DUPED: %s\n", w->buf);
+        assert(w != NULL && "Malloc failed");
+
+        words[words_len++] = w;
+
+        // Skip empty strings
+        for (; *p != '\0' && isspace(*p); p++)
+            end++;
+        
+        if (*p == '\0')
+            break;
+
+        start = end;
+    }
+
+    *wordlist = words;
+    return words_len;
+}
+
+void free_words(string ***wordlist, size_t nwords)
+{
+    string **words = *wordlist;
+    for (size_t i = 0; i < nwords; i++)
+        string_free(&words[i]);
+
+    free(words);
+    *wordlist = NULL;
 }
